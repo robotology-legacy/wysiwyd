e4b44968a7abeacedaafc0d73fa8dbc3c3382de8
Ugo Pattacini <ugo.pattacini@iit.it>
2014-03-20 16:09:51 +0100

    added operations to generate doxy files from xml

--- admin/scripts/doc-compile.sh
+++ admin/scripts/doc-compile.sh
@@ -3,6 +3,7 @@
 SOURCE=$PWD
 
 EXPECTED_ARGS=1
+DESTINATIONXML=generated-from-xml
  
 #if [ $# -ne $EXPECTED_ARGS ];
 #then
@@ -10,7 +11,22 @@ EXPECTED_ARGS=1
 #else
     base=$PWD
     cd $WYSIWYD_ROOT/main
+	
+	# clean-up
     rm tmp-doc -rf
+	rm $DESTINATIONXML -rf
+   
+    # generate doxy from xml
+    mkdir $DESTINATIONXML
+    list=`find . -iname *.xml | xargs`
+    for i in $list
+    do
+       path=`dirname $i`
+       filename=`basename $i`
+       doxyfile=${filename%%.*}
+       xsltproc --output $DESTINATIONXML/$doxyfile.dox $YARP_ROOT/scripts/yarp-module.xsl $i
+    done 
+
     doxygen ./conf/Doxyfile.txt
     cd $base
 #fi
