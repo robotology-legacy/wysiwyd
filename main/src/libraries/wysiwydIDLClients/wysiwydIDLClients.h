e67e0c49da2e5d5951ebc3fefec5b4812495bc0f
Ugo Pattacini <ugo.pattacini@iit.it>
2014-03-21 11:14:42 +0100

    [wysiwydIDLClients]: added master header file

---
+++ main/src/libraries/wysiwydIDLClients/wysiwydIDLClients.h
@@ -0,0 +1,41 @@
+/* 
+ * Copyright (C) 2014 WYSIWYD Consortium, European Commission FP7 Project ICT-612139
+ * Authors: Ugo Pattacini
+ * email:   ugo.pattacini@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * wysiwyd/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#ifndef __wysiwydIDLClients_H__
+#define __wysiwydIDLClients_H__
+
+#include "slidingController_IDL.h"
+
+/**
+*
+* @defgroup wysiwyd_wysiwydIDLClients IDL Client Interfaces
+* @ingroup wysiwyd_libraries 
+*  
+* This library contains all the IDL definitions for a client 
+* to interface to the services made available by WYSIWYD 
+* software. 
+*  
+*/
+
+/**
+* @ingroup wysiwyd_wysiwydIDLClients
+*/
+class slidingController_IDL;
+
+
+#endif
+
