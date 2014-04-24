a3cec27fac0a69a77b6994bc78d7b5ffea57d6e4
stephane-lallee <stephane.lallee@gmail.com>
2014-04-24 02:10:38 +0200

    [imageSplitter] refactored code, added fovea

--- main/src/modules/imageSplitter/main.cpp
+++ main/src/modules/imageSplitter/main.cpp
@@ -25,16 +25,107 @@
 
 using namespace yarp::os;
 using namespace yarp::sig;
-
 using namespace std;
+
+class Retina
+{
+	vector< vector<BufferedPort<ImageOf<PixelRgb> >* > > ports;
+	vector< vector< ImageOf<PixelRgb> > > preparedImgs;
+public:
+	Retina(std::string name, int w, int h)
+	{
+		ports.resize(w);
+		preparedImgs.resize(w);
+		for (int x = 0; x < w; x++)
+		{
+			ports[x].resize(h);
+			preparedImgs[x].resize(h);
+			for (int y = 0; y < h; y++)
+			{
+				stringstream ss;
+				ss << "/" << name << "/split/" << x << "_" << y << ":o";
+				ports[x][y] = new BufferedPort<ImageOf<PixelRgb> >();
+				ports[x][y]->open(ss.str().c_str());
+				preparedImgs[x][y] = ports[x][y]->prepare();
+			}
+		}
+	}
+
+	void createCvWindows(int xpos, int ypos)
+	{
+		for (int x = 0; x < ports.size(); x++)
+		{
+			for (int y = 0; y < ports[x].size(); y++)
+			{
+				cvNamedWindow(ports[x][y]->getName().c_str(), CV_WINDOW_AUTOSIZE);
+				cvMoveWindow(ports[x][y]->getName().c_str(), xpos + 300 * x, ypos + 300 * y);
+			}
+		}
+	}
+
+	void writeAndShow(IplImage* wholeImage, bool refreshGui)
+	{
+		IplImage* img;
+		if (wholeImage->roi != NULL)
+			img = cvCreateImage(cvSize(wholeImage->roi->width, wholeImage->roi->height), wholeImage->depth, wholeImage->nChannels);
+		else
+			img = cvCreateImage(cvSize(wholeImage->width, wholeImage->height), wholeImage->depth, wholeImage->nChannels);
+		cvCopyImage(wholeImage, img);
+		cvCvtColor(img, img, CV_BGR2RGB);
+		int rectW = img->width / ports.size();
+		int rectH = img->height / ports[0].size();
+		
+		for (int x = 0; x < ports.size(); x++)
+		{
+			for (int y = 0; y < ports[x].size(); y++)
+			{
+				cvSetImageROI(img, cvRect(x*rectW, y*rectH, rectW, rectH));
+				ImageOf<PixelRgb> &imgYarp = ports[x][y]->prepare();
+				imgYarp.resize(rectW, rectH);
+				cvCopy(img, (IplImage*)imgYarp.getIplImage(), NULL);
+				if (refreshGui)
+					cvShowImage(ports[x][y]->getName().c_str(), img);
+			}
+		}
+
+		cvWaitKey(1);
+		cvReleaseImage(&img);
+
+		//write all at once to avoid diagonal delay
+		for (int x = 0; x < ports.size(); x++)
+		{
+			for (int y = 0; y < ports[x].size(); y++)
+			{
+				ports[x][y]->write(true);
+			}
+		}
+	}
+	
+	bool close()
+	{
+		for (int x = 0; x < ports.size(); x++)
+		{
+			for (int y = 0; y < ports[x].size(); y++)
+			{
+				ports[x][y]->interrupt();
+				ports[x][y]->close();
+				delete ports[x][y];
+			}
+		}
+		return true;
+	}
+};
 
 class ImageSplitter : public yarp::os::RFModule
 {
 	BufferedPort<ImageOf<PixelRgb> > inPort;
+	double foveaRatio;
 	bool showImages;
 
 public:
-	vector< vector<BufferedPort<ImageOf<PixelRgb> >* > > ports;
+	Retina* global;
+	Retina* fovea;
+
 	bool configure(ResourceFinder &rf)
 	{
 		string name = rf.check("name", Value("imageSplitter")).asString();
@@ -44,37 +135,17 @@ public:
 
 		int splitW = rf.check("splitW", Value(3)).asInt();
 		int splitH = rf.check("splitH", Value(3)).asInt();
+		int splitFovea = rf.check("splitFovea", Value(1)).asInt();
+		foveaRatio = rf.check("foveaRatio", Value(0.3)).asDouble();
 		showImages = rf.check("showImages");
 
-		ports.resize(splitW);
-		for (int x = 0; x < splitW; x++)
-		{
-			ports[x].resize(splitH);
-			for (int y = 0; y < splitH; y++)
-			{
-				stringstream ss;
-				ss <<"/"<< getName().c_str() << "/split/" << x << "_" << y << ":o";
-				ports[x][y] = new BufferedPort<ImageOf<PixelRgb> >();
-				ports[x][y]->open(ss.str().c_str());
-			}
-		}
-
-
+		//Create ports
+		global = new Retina("split", splitW, splitH);
+		fovea = new Retina("fovea/split", splitFovea, splitFovea);
 		if (showImages)
 		{
-			int xpos = 0;
-			int ypos = 0;
-			for (int x = 0; x < splitW; x++)
-			{
-				ports[x].resize(splitH);
-				for (int y = 0; y < splitH; y++)
-				{
-					stringstream ss;
-					ss << "/" << getName().c_str() << "/split/" << x << "_" << y << ":o";
-					cvNamedWindow(ss.str().c_str(), CV_WINDOW_AUTOSIZE);
-					cvMoveWindow(ss.str().c_str(), xpos + 300 * x, ypos + 300*y);
-				}
-			}
+			global->createCvWindows(0, 0);
+			fovea->createCvWindows(640, 0);
 		}
 		return true;
 	}
@@ -89,52 +160,28 @@ public:
 		ImageOf<PixelRgb> *img = inPort.read(false);
 		if (img)
 		{
-			splitNsend((IplImage*)img->getIplImage());
+			IplImage* cvImg = (IplImage*)img->getIplImage();
+			global->writeAndShow(cvImg, showImages);
+			
+			//Create the input for the fovea
+			IplImage* foveaImg = cvCreateImage(cvSize(cvImg->width, cvImg->height), cvImg->depth, cvImg->nChannels);
+			cvCopyImage(cvImg, foveaImg);
+			cvSetImageROI(foveaImg, cvRect(
+				foveaImg->width / 2, 
+				foveaImg->height / 2, 
+				foveaRatio*foveaImg->width, 
+				foveaRatio*foveaImg->height));
+			fovea->writeAndShow(foveaImg, showImages);
+			cvReleaseImage(&foveaImg);
 		}
 		return true;
-	}
-
-	void splitNsend(IplImage* img)
-	{
-		cvCvtColor(img, img, CV_BGR2RGB);
-		int rectW = img->width / ports.size();
-		int rectH = img->height / ports[0].size();
-		IplImage *img2 = cvCreateImage(cvSize(rectW,rectH), img->depth, img->nChannels);
-		for (int x = 0; x < ports.size(); x++)
-		{
-			for (int y = 0; y < ports[x].size(); y++)
-			{
-				cvSetImageROI(img, cvRect(x*rectW, y*rectH, rectW, rectH));
-				cvCopy(img, img2, NULL);
-				ImageOf<PixelRgb> &imgYarp = ports[x][y]->prepare();
-				imgYarp.wrapIplImage(img2);
-				ports[x][y]->write(true); 
-				
-				if (showImages)
-				{
-					stringstream ss;
-					ss << "/" << getName().c_str() << "/split/" << x << "_" << y << ":o";
-					cvShowImage(ss.str().c_str(), img2);
-					cvWaitKey(1);
-				}
-				cvResetImageROI(img);
-			}
-		}
-		cvReleaseImage(&img2);
 	}
 
 	bool close()
 	{
-		for (int x = 0; x < ports.size(); x++)
-		{
-			for (int y = 0; y < ports[x].size(); y++)
-			{
-				ports[x][y]->interrupt();
-				ports[x][y]->close();
-				delete ports[x][y];
-			}
-		}
-		return true;
+		bool result = global->close();
+		result &= fovea->close();
+		return result;
 	}
 };
 
