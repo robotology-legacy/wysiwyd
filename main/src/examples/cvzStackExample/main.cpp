b797d1c2fd6bd0c113cdd36ad6afcc62c5e20538
stephane-lallee <stephane.lallee@gmail.com>
2014-05-07 17:48:05 +0200

    Reorganisation
    Added an example, reorganizing a bit

---
+++ main/src/examples/cvzStackExample/main.cpp
@@ -0,0 +1,60 @@
+#include "cvz/core/all.h"
+#include "cvz/gui/all.h"
+
+#include <yarp/os/all.h>
+
+using namespace std;
+using namespace yarp::os;
+
+int main(int argc, char * argv[])
+{
+	Network yarp;
+	bool b1, b2;
+	cout << "Server name is : " << yarp.getNameServerName() << endl;
+	//if (!yarp.setNameServerName("/test"))
+	//	cout << "Impossible to force name server!" << endl;
+	//else
+	//	cout << "Forced name server to be /test" << endl;
+
+	yarp.detectNameServer(true, b1, b2);
+
+	if (!Network::checkNetwork())
+	{
+		cout << "yarp network is not available!" << endl;
+		return 0;
+	}
+
+	ResourceFinder rf;
+	rf.setVerbose(true);
+	rf.setDefaultContext("cvz");
+	rf.setDefaultConfigFile("default.ini"); //overridden by --from parameter
+	rf.configure(argc, argv);
+
+	cvz::core::CvzStack stack;
+
+
+	//Add head proprioception
+	stack.addCvz("stackExample_head.ini", "head");
+
+	//Add vision
+	stack.addCvz("stackExample_vision.ini", "vision");
+
+	//Add multimodal map
+	stack.addCvz("stackExample_multimodal.ini", "multimodal");
+
+	//Connect the head and vision to the multimodal map
+	stack.connectModalities("/head/multimodal", "/multimodal/gaze");
+	stack.connectModalities("/vision/multimodal", "/multimodal/vision");
+
+
+
+	//Make sure the graph is completed and all the connections are established
+	stack.connectModalities();
+
+	//Instantiate the module, each cvz will be spawn as a thread
+	stack.configure(rf);
+
+	//Run the module
+	stack.runModule();
+	return 0;
+}
