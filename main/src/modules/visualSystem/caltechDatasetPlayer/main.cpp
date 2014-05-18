13e51da1dac711e1e78aa6ea478754b0bc191862
Ugo Pattacini <ugo.pattacini@iit.it>
2014-05-18 14:27:34 +0200

    fixed formatting and some warnings

--- main/src/modules/visualSystem/caltechDatasetPlayer/main.cpp
+++ main/src/modules/visualSystem/caltechDatasetPlayer/main.cpp
@@ -35,148 +35,150 @@ using namespace std;
 
 class Player : public RFModule
 {
-	BufferedPort<ImageOf<PixelRgb> > portImg;
-	Port portCategoryTag;
-	Port portCategoryVector;
-	map< string, vector< IplImage* > > buffer;
+    BufferedPort<ImageOf<PixelRgb> > portImg;
+    Port portCategoryTag;
+    Port portCategoryVector;
+    map< string, vector< IplImage* > > buffer;
 
 public:
 
-	bool configure(yarp::os::ResourceFinder &rf)
-	{
-		string name = rf.check("name", Value("caltechDatasetPlayer")).asString();
-		string dsPath = rf.check("dataPath", Value(rf.findPath("default.ini").c_str())).asString();
-		dsPath.erase(dsPath.end() - 11, dsPath.end());
-		setName(name.c_str());
+    bool configure(yarp::os::ResourceFinder &rf)
+    {
+        string name = rf.check("name", Value("caltechDatasetPlayer")).asString();
+        string dsPath = rf.check("dataPath", Value(rf.findPath("default.ini").c_str())).asString();
+        dsPath.erase(dsPath.end() - 11, dsPath.end());
+        setName(name.c_str());
 
-		//Load the dataset
-		std::cout << "Looking for dataset in : " << dsPath << std::endl;
-		string currentPath = dsPath;
-		DIR *dirp = NULL;
-		struct dirent *direntp = NULL;
+        //Load the dataset
+        std::cout << "Looking for dataset in : " << dsPath << std::endl;
+        string currentPath = dsPath;
+        DIR *dirp = NULL;
+        struct dirent *direntp = NULL;
 
-		/* Check if file is opened */
-		if ((dirp = opendir(currentPath.c_str())) == NULL)
-		{
-			fprintf(stdout, "Error( %d opening %s\n", errno, currentPath.c_str());
-			return errno;
-		}
+        /* Check if file is opened */
+        if ((dirp = opendir(currentPath.c_str())) == NULL)
+        {
+            fprintf(stdout, "Error( %d opening %s\n", errno, currentPath.c_str());
+            return false;
+        }
 
-		while ((direntp = readdir(dirp)) != NULL)
-		{
-			/* Ignore special directories. */
-			if ((strcmp(direntp->d_name, ".") == 0) ||
-				(strcmp(direntp->d_name, "..") == 0))
-				continue;
+        while ((direntp = readdir(dirp)) != NULL)
+        {
+            /* Ignore special directories. */
+            if ((strcmp(direntp->d_name, ".") == 0) ||
+                (strcmp(direntp->d_name, "..") == 0))
+                continue;
 
-			string category = direntp->d_name;
-			string fullPath = currentPath + "\\" + category;
-			cout << "Category : " << category << endl;
-			//if (
-			//	category != "brain" &&
-			//	category != "dolphin" &&
-			//	category != "crab" &&
-			//	category != "barrel" &&
-			//	category != "faces"
-			//	)continue;
+            string category = direntp->d_name;
+            string fullPath = currentPath + "\\" + category;
+            cout << "Category : " << category << endl;
+            //if (
+            //  category != "brain" &&
+            //  category != "dolphin" &&
+            //  category != "crab" &&
+            //  category != "barrel" &&
+            //  category != "faces"
+            //  )continue;
 
-			DIR *dirCat = opendir(fullPath.c_str());
-			struct dirent *direntFile = NULL;
-			int cntCategory = 0;
-			while ((direntFile = readdir(dirCat)))
-			{
-				/* Ignore special directories. */
-				if ((strcmp(direntFile->d_name, ".") == 0) ||
-					(strcmp(direntFile->d_name, "..") == 0))
-					continue;
+            DIR *dirCat = opendir(fullPath.c_str());
+            struct dirent *direntFile = NULL;
+            int cntCategory = 0;
+            while ((direntFile = readdir(dirCat)))
+            {
+                /* Ignore special directories. */
+                if ((strcmp(direntFile->d_name, ".") == 0) ||
+                    (strcmp(direntFile->d_name, "..") == 0))
+                    continue;
 
 
-				cout << ".";
-				string fileName = direntFile->d_name;
-				string fileNamePath = fullPath + "\\" + fileName;
+                cout << ".";
+                string fileName = direntFile->d_name;
+                string fileNamePath = fullPath + "\\" + fileName;
 
-				IplImage* pImg;
-				IplImage* tmpImg = cvLoadImage(fileNamePath.c_str(), CV_LOAD_IMAGE_UNCHANGED);
-				pImg = cvCreateImage(cvSize(300, 200), tmpImg->depth, tmpImg->nChannels);
-				cvResize(tmpImg, pImg);
-				buffer[category].push_back(pImg);
-				cvReleaseImage(&tmpImg);
-				cntCategory++;
-			}
-			cout << "Category : " << category << " has " << cntCategory << " items" << endl;
-		}
-		cout << "Total items : " << buffer.size() << endl;
+                IplImage* pImg;
+                IplImage* tmpImg = cvLoadImage(fileNamePath.c_str(), CV_LOAD_IMAGE_UNCHANGED);
+                pImg = cvCreateImage(cvSize(300, 200), tmpImg->depth, tmpImg->nChannels);
+                cvResize(tmpImg, pImg);
+                buffer[category].push_back(pImg);
+                cvReleaseImage(&tmpImg);
+                cntCategory++;
+            }
+            cout << "Category : " << category << " has " << cntCategory << " items" << endl;
+        }
+        cout << "Total items : " << buffer.size() << endl;
 
-		portImg.open("/" + name + "/image:o");
-		portCategoryTag.open("/" + name + "/categoryTag:o");
-		portCategoryVector.open("/" + name + "/categoryVector:o");
-	}
+        portImg.open("/" + name + "/image:o");
+        portCategoryTag.open("/" + name + "/categoryTag:o");
+        portCategoryVector.open("/" + name + "/categoryVector:o");
 
-	bool updateModule()
-	{		
-		//Pick a random category
-		int countIt = yarp::os::Random::uniform(0, buffer.size() - 1);
-		map<string, vector< IplImage*> >::iterator it = buffer.begin();
-		for (int i = 0; i < countIt; i++)
-		{
-			it++;
-		}
+        return true;
+    }
 
-		//Pick rnd image
-		int rndImg = yarp::os::Random::uniform(0, it->second.size() - 1);
+    bool updateModule()
+    {       
+        //Pick a random category
+        int countIt = yarp::os::Random::uniform(0, buffer.size() - 1);
+        map<string, vector< IplImage*> >::iterator it = buffer.begin();
+        for (int i = 0; i < countIt; i++)
+        {
+            it++;
+        }
 
-		//Send
-		ImageOf<PixelRgb> &img = portImg.prepare();
-		img.wrapIplImage(it->second[rndImg]);
-		portImg.write();
+        //Pick rnd image
+        int rndImg = yarp::os::Random::uniform(0, it->second.size() - 1);
 
-		//Category tag
-		Bottle botTag;
-		//bot.addString(it->first);
-		botTag.addString(it->first);
-		portCategoryTag.write(botTag);
+        //Send
+        ImageOf<PixelRgb> &img = portImg.prepare();
+        img.wrapIplImage(it->second[rndImg]);
+        portImg.write();
 
-		Bottle botVect;
-		for (int i = 0; i < buffer.size(); i++)
-			botVect.addInt( countIt==i );
-		portCategoryVector.write(botVect);
-		return true;
-	}
+        //Category tag
+        Bottle botTag;
+        //bot.addString(it->first);
+        botTag.addString(it->first);
+        portCategoryTag.write(botTag);
 
-	bool close()
-	{		
-		//close ports
-		portImg.close();
-		portCategoryTag.close();
-		portCategoryVector.close();
+        Bottle botVect;
+        for (int i = 0; i < buffer.size(); i++)
+            botVect.addInt( countIt==i );
+        portCategoryVector.write(botVect);
+        return true;
+    }
 
-		//Release images
-		for (map<string, vector< IplImage*> >::iterator it = buffer.begin(); it != buffer.end(); it++)
-			for (int i = 0; i < it->second.size(); i++)
-				cvReleaseImage(&it->second[i]);
+    bool close()
+    {       
+        //close ports
+        portImg.close();
+        portCategoryTag.close();
+        portCategoryVector.close();
 
-		return true;
-	}
+        //Release images
+        for (map<string, vector< IplImage*> >::iterator it = buffer.begin(); it != buffer.end(); it++)
+            for (int i = 0; i < it->second.size(); i++)
+                cvReleaseImage(&it->second[i]);
+
+        return true;
+    }
 };
 
 
 int main(int argc, char *argv[]) 
 {
-	
-	Network yarp;
-	if (!Network::checkNetwork())
-	{
-		cout << "yarp network is not available!" << endl;
-		return 0;
-	}
+    
+    Network yarp;
+    if (!Network::checkNetwork())
+    {
+        cout << "yarp network is not available!" << endl;
+        return 0;
+    }
 
-	ResourceFinder rf;
-	rf.setVerbose(true);
-	rf.setDefaultContext("caltechDataset");
-	rf.setDefaultConfigFile("default.ini"); //overridden by --from parameter
-	rf.configure(argc, argv);
+    ResourceFinder rf;
+    rf.setVerbose(true);
+    rf.setDefaultContext("caltechDataset");
+    rf.setDefaultConfigFile("default.ini"); //overridden by --from parameter
+    rf.configure(argc, argv);
 
-	Player mod;
-	mod.configure(rf);
-	return mod.runModule();;
+    Player mod;
+    mod.configure(rf);
+    return mod.runModule();;
 }
--- main/src/modules/visualSystem/imageSplitter/main.cpp
+++ main/src/modules/visualSystem/imageSplitter/main.cpp
@@ -29,177 +29,177 @@ using namespace std;
 
 class Retina
 {
-	vector< vector<BufferedPort<ImageOf<PixelRgb> >* > > ports;
-	vector< vector< ImageOf<PixelRgb> > > preparedImgs;
+    vector< vector<BufferedPort<ImageOf<PixelRgb> >* > > ports;
+    vector< vector< ImageOf<PixelRgb> > > preparedImgs;
 public:
-	Retina(std::string name, int w, int h)
-	{
-		ports.resize(w);
-		preparedImgs.resize(w);
-		for (int x = 0; x < w; x++)
-		{
-			ports[x].resize(h);
-			preparedImgs[x].resize(h);
-			for (int y = 0; y < h; y++)
-			{
-				stringstream ss;
-				ss << "/" << name << "/split/" << x << "_" << y << ":o";
-				ports[x][y] = new BufferedPort<ImageOf<PixelRgb> >();
-				ports[x][y]->open(ss.str().c_str());
-				preparedImgs[x][y] = ports[x][y]->prepare();
-			}
-		}
-	}
+    Retina(std::string name, int w, int h)
+    {
+        ports.resize(w);
+        preparedImgs.resize(w);
+        for (int x = 0; x < w; x++)
+        {
+            ports[x].resize(h);
+            preparedImgs[x].resize(h);
+            for (int y = 0; y < h; y++)
+            {
+                stringstream ss;
+                ss << "/" << name << "/split/" << x << "_" << y << ":o";
+                ports[x][y] = new BufferedPort<ImageOf<PixelRgb> >();
+                ports[x][y]->open(ss.str().c_str());
+                preparedImgs[x][y] = ports[x][y]->prepare();
+            }
+        }
+    }
 
-	void createCvWindows(int xpos, int ypos)
-	{
-		for (int x = 0; x < ports.size(); x++)
-		{
-			for (int y = 0; y < ports[x].size(); y++)
-			{
-				cvNamedWindow(ports[x][y]->getName().c_str(), CV_WINDOW_AUTOSIZE);
-				cvMoveWindow(ports[x][y]->getName().c_str(), xpos + 300 * x, ypos + 300 * y);
-			}
-		}
-	}
+    void createCvWindows(int xpos, int ypos)
+    {
+        for (int x = 0; x < ports.size(); x++)
+        {
+            for (int y = 0; y < ports[x].size(); y++)
+            {
+                cvNamedWindow(ports[x][y]->getName().c_str(), CV_WINDOW_AUTOSIZE);
+                cvMoveWindow(ports[x][y]->getName().c_str(), xpos + 300 * x, ypos + 300 * y);
+            }
+        }
+    }
 
-	void writeAndShow(IplImage* wholeImage, bool refreshGui)
-	{
-		IplImage* img;
-		if (wholeImage->roi != NULL)
-			img = cvCreateImage(cvSize(wholeImage->roi->width, wholeImage->roi->height), wholeImage->depth, wholeImage->nChannels);
-		else
-			img = cvCreateImage(cvSize(wholeImage->width, wholeImage->height), wholeImage->depth, wholeImage->nChannels);
-		cvCopyImage(wholeImage, img);
-		cvCvtColor(img, img, CV_BGR2RGB);
-		int rectW = img->width / ports.size();
-		int rectH = img->height / ports[0].size();
-		
-		for (int x = 0; x < ports.size(); x++)
-		{
-			for (int y = 0; y < ports[x].size(); y++)
-			{
-				cvSetImageROI(img, cvRect(x*rectW, y*rectH, rectW, rectH));
-				ImageOf<PixelRgb> &imgYarp = ports[x][y]->prepare();
-				imgYarp.resize(rectW, rectH);
-				cvCopy(img, (IplImage*)imgYarp.getIplImage(), NULL);
-				if (refreshGui)
-					cvShowImage(ports[x][y]->getName().c_str(), img);
-			}
-		}
+    void writeAndShow(IplImage* wholeImage, bool refreshGui)
+    {
+        IplImage* img;
+        if (wholeImage->roi != NULL)
+            img = cvCreateImage(cvSize(wholeImage->roi->width, wholeImage->roi->height), wholeImage->depth, wholeImage->nChannels);
+        else
+            img = cvCreateImage(cvSize(wholeImage->width, wholeImage->height), wholeImage->depth, wholeImage->nChannels);
+        cvCopyImage(wholeImage, img);
+        cvCvtColor(img, img, CV_BGR2RGB);
+        int rectW = img->width / ports.size();
+        int rectH = img->height / ports[0].size();
+        
+        for (int x = 0; x < ports.size(); x++)
+        {
+            for (int y = 0; y < ports[x].size(); y++)
+            {
+                cvSetImageROI(img, cvRect(x*rectW, y*rectH, rectW, rectH));
+                ImageOf<PixelRgb> &imgYarp = ports[x][y]->prepare();
+                imgYarp.resize(rectW, rectH);
+                cvCopy(img, (IplImage*)imgYarp.getIplImage(), NULL);
+                if (refreshGui)
+                    cvShowImage(ports[x][y]->getName().c_str(), img);
+            }
+        }
 
-		cvWaitKey(1);
-		cvReleaseImage(&img);
+        cvWaitKey(1);
+        cvReleaseImage(&img);
 
-		//write all at once to avoid diagonal delay
-		for (int x = 0; x < ports.size(); x++)
-		{
-			for (int y = 0; y < ports[x].size(); y++)
-			{
-				ports[x][y]->write(true);
-			}
-		}
-	}
-	
-	bool close()
-	{
-		for (int x = 0; x < ports.size(); x++)
-		{
-			for (int y = 0; y < ports[x].size(); y++)
-			{
-				ports[x][y]->interrupt();
-				ports[x][y]->close();
-				delete ports[x][y];
-			}
-		}
-		return true;
-	}
+        //write all at once to avoid diagonal delay
+        for (int x = 0; x < ports.size(); x++)
+        {
+            for (int y = 0; y < ports[x].size(); y++)
+            {
+                ports[x][y]->write(true);
+            }
+        }
+    }
+    
+    bool close()
+    {
+        for (int x = 0; x < ports.size(); x++)
+        {
+            for (int y = 0; y < ports[x].size(); y++)
+            {
+                ports[x][y]->interrupt();
+                ports[x][y]->close();
+                delete ports[x][y];
+            }
+        }
+        return true;
+    }
 };
 
 class ImageSplitter : public yarp::os::RFModule
 {
-	BufferedPort<ImageOf<PixelRgb> > inPort;
-	double foveaRatio;
-	bool showImages;
+    BufferedPort<ImageOf<PixelRgb> > inPort;
+    double foveaRatio;
+    bool showImages;
 
 public:
-	Retina* global;
-	Retina* fovea;
+    Retina* global;
+    Retina* fovea;
 
-	bool configure(ResourceFinder &rf)
-	{
-		string name = rf.check("name", Value("imageSplitter")).asString();
-		setName(name.c_str());
+    bool configure(ResourceFinder &rf)
+    {
+        string name = rf.check("name", Value("imageSplitter")).asString();
+        setName(name.c_str());
 
-		inPort.open(("/" + name + "/image:i").c_str());
+        inPort.open(("/" + name + "/image:i").c_str());
 
-		int splitW = rf.check("splitW", Value(3)).asInt();
-		int splitH = rf.check("splitH", Value(3)).asInt();
-		int splitFovea = rf.check("splitFovea", Value(1)).asInt();
-		foveaRatio = rf.check("foveaRatio", Value(0.3)).asDouble();
-		showImages = rf.check("showImages");
+        int splitW = rf.check("splitW", Value(3)).asInt();
+        int splitH = rf.check("splitH", Value(3)).asInt();
+        int splitFovea = rf.check("splitFovea", Value(1)).asInt();
+        foveaRatio = rf.check("foveaRatio", Value(0.3)).asDouble();
+        showImages = rf.check("showImages");
 
-		//Create ports
-		global = new Retina(name, splitW, splitH);
-		fovea = new Retina(name+"/fovea", splitFovea, splitFovea);
-		if (showImages)
-		{
-			global->createCvWindows(0, 0);
-			fovea->createCvWindows(640, 0);
-		}
-		return true;
-	}
+        //Create ports
+        global = new Retina(name, splitW, splitH);
+        fovea = new Retina(name+"/fovea", splitFovea, splitFovea);
+        if (showImages)
+        {
+            global->createCvWindows(0, 0);
+            fovea->createCvWindows(640, 0);
+        }
+        return true;
+    }
 
-	double getPeriod()
-	{
-		return 0.01;
-	}
+    double getPeriod()
+    {
+        return 0.01;
+    }
 
-	bool updateModule()
-	{
-		ImageOf<PixelRgb> *img = inPort.read(false);
-		if (img)
-		{
-			IplImage* cvImg = (IplImage*)img->getIplImage();
-			global->writeAndShow(cvImg, showImages);
-			
-			//Create the input for the fovea
-			IplImage* foveaImg = cvCreateImage(cvSize(cvImg->width, cvImg->height), cvImg->depth, cvImg->nChannels);
-			cvCopyImage(cvImg, foveaImg);
-			cvSetImageROI(foveaImg, cvRect(
-				foveaImg->width / 2 - (int)(foveaRatio*foveaImg->width)/2.0,
-				foveaImg->height / 2 - (int)(foveaRatio*foveaImg->height)/2.0,
-				(int)(foveaRatio*foveaImg->width), 
-				(int)(foveaRatio*foveaImg->height)));
-			fovea->writeAndShow(foveaImg, showImages);
-			cvReleaseImage(&foveaImg);
-		}
-		return true;
-	}
+    bool updateModule()
+    {
+        ImageOf<PixelRgb> *img = inPort.read(false);
+        if (img)
+        {
+            IplImage* cvImg = (IplImage*)img->getIplImage();
+            global->writeAndShow(cvImg, showImages);
+            
+            //Create the input for the fovea
+            IplImage* foveaImg = cvCreateImage(cvSize(cvImg->width, cvImg->height), cvImg->depth, cvImg->nChannels);
+            cvCopyImage(cvImg, foveaImg);
+            cvSetImageROI(foveaImg, cvRect(
+                foveaImg->width / 2 - (int)(foveaRatio*foveaImg->width)/2.0,
+                foveaImg->height / 2 - (int)(foveaRatio*foveaImg->height)/2.0,
+                (int)(foveaRatio*foveaImg->width), 
+                (int)(foveaRatio*foveaImg->height)));
+            fovea->writeAndShow(foveaImg, showImages);
+            cvReleaseImage(&foveaImg);
+        }
+        return true;
+    }
 
-	bool close()
-	{
-		bool result = global->close();
-		result &= fovea->close();
-		return result;
-	}
+    bool close()
+    {
+        bool result = global->close();
+        result &= fovea->close();
+        return result;
+    }
 };
 
 int main(int argc, char *argv[]) 
 {
-	Network yarp;
-	if (!yarp.checkNetwork())
-	{
-		printf("yarp network is not available!\n");
-		return -1;
-	}
+    Network yarp;
+    if (!yarp.checkNetwork())
+    {
+        printf("yarp network is not available!\n");
+        return -1;
+    }
 
-	ResourceFinder rf;
-	rf.setDefaultContext("imageSplitter");
+    ResourceFinder rf;
+    rf.setDefaultContext("imageSplitter");
 
-	rf.configure(argc, argv);
+    rf.configure(argc, argv);
 
-	ImageSplitter mod;
-	return mod.runModule(rf);
+    ImageSplitter mod;
+    return mod.runModule(rf);
     return 0;
 }
