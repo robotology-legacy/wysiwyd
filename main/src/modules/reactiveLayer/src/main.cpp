30d8b4716c073da6863048aa089562b9ff6d77c4
Stephane Lallee <stephane.lallee@gmail.com>
2014-07-24 16:09:11 +0200

    Towards VVV2014 demo

--- main/src/modules/reactiveLayer/src/main.cpp
+++ main/src/modules/reactiveLayer/src/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char * argv[])
     ReactiveLayer mod;
     ResourceFinder rf;
     rf.setVerbose(true);
-    rf.setDefaultContext("reactiveLayer/conf");
+    rf.setDefaultContext("reactiveLayer");
     rf.setDefaultConfigFile("default.ini");
     rf.configure( argc, argv);
     mod.runModule(rf);
