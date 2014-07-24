79d4bc57e58600c6794e527d1599194e3491134e
maxime-petit <m.petit@imperial.ac.uk>
2014-07-24 17:42:07 +0200

    move agentDetector location
    move agentDetector from systemVisual to humanUnderstanding

--- main/src/modules/humanUnderstanding/agentDetector/main.cpp
+++ main/src/modules/humanUnderstanding/agentDetector/main.cpp
@@ -1,14 +1,14 @@
 /*
- * Copyright (C) 2012 EFAA Consortium, European Commission FP7 Project IST-270490
- * Authors: Stéphane Lallée
+ * Copyright (C) 2014 WYSIWYD Consortium, European Commission FP7 Project ICT-612139
+ * Authors: Stéphane Lallée, moved from EFAA by Maxime Petit
  * email:   stephane.lallee@gmail.com
- * website: http://efaa.upf.edu/
+ * website: http://wysiwyd.upf.edu/ 
  * Permission is granted to copy, distribute, and/or modify this program
  * under the terms of the GNU General Public License, version 2 or any
  * later version published by the Free Software Foundation.
  *
  * A copy of the license can be found at
- * $EFAA_ROOT/license/gpl.txt
+ * $WYSIWYD_ROOT/license/gpl.txt
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -73,9 +73,9 @@ int main(int argc, char *argv[])
 
     ResourceFinder rf;
     rf.setVerbose(true);
-    rf.setDefaultContext("agentDetector/conf");
-    rf.setDefaultConfigFile("default.ini");
-    rf.configure("EFAA_ROOT",argc,argv);
+    rf.setDefaultContext("agentDetector");
+    rf.setDefaultConfigFile("agentDetector.ini");
+    rf.configure(argc,argv);
 
     AgentDetector mod;
 
