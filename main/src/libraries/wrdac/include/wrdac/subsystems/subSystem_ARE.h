b401d391a493585f1a8dc5a0cfd7916029c962e8
Ugo Pattacini <ugo.pattacini@iit.it>
2014-08-08 18:48:57 +0200

    [ARE subsystem]: restyling

--- main/src/libraries/wrdac/include/wrdac/subsystems/subSystem_ARE.h
+++ main/src/libraries/wrdac/include/wrdac/subsystems/subSystem_ARE.h
@@ -34,13 +34,17 @@ namespace wrdac {
 /**
 * \ingroup wrdac_clients
 *
-* SubSystem for actionsRenderingEngine (a.k.a. ARE).
+* SubSystem for actionsRenderingEngine (a.k.a. ARE). 
+*  
+* Please refer to 
+* http://wiki.icub.org/iCub_documentation/group__actionsRenderingEngine.html 
 */
 class SubSystem_ARE : public SubSystem
 {
 private:
     yarp::os::Port cmdPort;
 
+    /********************************************************************************/
     void appendCartesianTarget(yarp::os::Bottle& b, const yarp::sig::Vector &t)
     {
         yarp::os::Bottle& sub = b.addList();
@@ -49,6 +53,7 @@ private:
             sub.addDouble(t[i]);
     }
 
+    /********************************************************************************/
     bool sendCmd(yarp::os::Bottle &cmd, const bool shouldWait)
     {        
         if (shouldWait)
@@ -64,18 +69,21 @@ private:
     }
 
 protected:
+    /********************************************************************************/
     virtual bool connect() 
     { 
         return yarp::os::Network::connect(cmdPort.getName(),"/actionsRenderingEngine/cmd:io");
     }
 
 public:
+    /********************************************************************************/
     SubSystem_ARE(const std::string &masterName) : SubSystem(masterName)
     {
         cmdPort.open(("/"+masterName+"/"+SUBSYSTEM_ARE+"/cmd:io").c_str());
         m_type = SUBSYSTEM_ARE;
     }
 
+    /********************************************************************************/
     virtual void Close() 
     { 
         cmdPort.interrupt();
@@ -83,7 +91,7 @@ public:
     }
 
     /**
-    * Robot goes to home position
+    * Go to home position
     * @param part (gaze, head, hands, finguers, all. All by default)
     * @param shouldWait is the function blocking?
     * @return true in case of successfull motor command, false 
@@ -98,10 +106,14 @@ public:
     }
 
     /**
-    * the robot tries to reach the specified [target] and grasp it. Optional parameter "side" or "above" can be supplied to choose the orientation the robot should try to mantain while performing the action (default: "above").
+    * Reach the specified [target] and grasp it. Optional parameter
+    * "side" or "above" can be supplied to choose the orientation 
+    * the robot should keep while performing the action (default: 
+    * "above"). 
     * @param target Target to grasp in cartesian coordinates
-    * @param opts Options of ARE commands ("no_head", "no_gaze", "no_sacc", "still", "left", "right" Please refer to http://wiki.icub.org/iCub_documentation/group__actionsRenderingEngine.html)
-    * @param shouldWait is the function blocking ? 
+    * @param opts Options of ARE commands ("no_head", "no_gaze", 
+    *             "no_sacc", "still", "left", "right").
+    * @param shouldWait is the function blocking? 
     * @return true in case of successfull motor command, false 
     *         otherwise.
     */
@@ -120,10 +132,14 @@ public:
     }
 
     /**
-    * the robot tries to reach the specified [target] from one side and then push it laterally. Optional parameter "away" can be supplied in order to have the robot push the object away from its root reference frame.
+    * Reach the specified [target] from one side and then push it
+    * laterally. Optional parameter "away" can be supplied in order 
+    * to have the robot push the object away from its root reference 
+    * frame. 
     * @param target Target to grasp in cartesian coordinates
-    * @param opts Options of ARE commands ("no_head", "no_gaze", "no_sacc", "still", "left", "right" Please refer to http://wiki.icub.org/iCub_documentation/group__actionsRenderingEngine.html)
-    * @param shouldWait is the function blocking ? 
+    * @param opts Options of ARE commands ("no_head", "no_gaze", 
+    *             "no_sacc", "still", "left", "right").
+    * @param shouldWait is the function blocking? 
     * @return true in case of successfull motor command, false 
     *         otherwise.
     */
@@ -142,10 +158,11 @@ public:
     }
 
     /**
-    * the robot tries to point the specified [target] with its index finger. 
+    * Point at the specified [target] with the index finger.
     * @param target Target to grasp in cartesian coordinates
-    * @param opts Options of ARE commands ("no_head", "no_gaze", "no_sacc", "still", "left", "right" Please refer to http://wiki.icub.org/iCub_documentation/group__actionsRenderingEngine.html)
-    * @param shouldWait is the function blocking ? 
+    * @param opts Options of ARE commands ("no_head", "no_gaze", 
+    *             "no_sacc", "still", "left", "right").
+    * @param shouldWait is the function blocking? 
     * @return true in case of successfull motor command, false 
     *         otherwise.
     */
@@ -164,9 +181,11 @@ public:
     }
 
     /**
-    * if the robot is holding an object it brings it over the table and drops it on a random position approximatively in front of it. 
-    * @param opts Options of ARE commands ("no_head", "no_gaze", "no_sacc", "still", "left", "right" Please refer to http://wiki.icub.org/iCub_documentation/group__actionsRenderingEngine.html)
-    * @param shouldWait is the function blocking ? 
+    * If an object is held, bring it over the table and drop it on a
+    * random position. 
+    * @param opts Options of ARE commands ("no_head", "no_gaze", 
+    *             "no_sacc", "still", "left", "right").
+    * @param shouldWait is the function blocking? 
     * @return true in case of successfull motor command, false 
     *         otherwise.
     */
@@ -184,10 +203,11 @@ public:
     }
 
     /**
-    * Drop the object on a given target
+    * Drop the object on a given target.
     * @param target Target where to drop in cartesian coordinates
-    * @param opts Options of ARE commands ("no_head", "no_gaze", "no_sacc", "still", "left", "right" Please refer to http://wiki.icub.org/iCub_documentation/group__actionsRenderingEngine.html)
-    * @param shouldWait is the function blocking ? 
+    * @param opts Options of ARE commands ("no_head", "no_gaze", 
+    *             "no_sacc", "still", "left", "right").
+    * @param shouldWait is the function blocking? 
     * @return true in case of successfull motor command, false 
     *         otherwise.
     */
@@ -207,9 +227,11 @@ public:
     }
 
     /**
-    * Bring the hand in the visual field and move it with the purpose of visual exploration.
-    * @param opts Options of ARE commands ("no_head", "no_gaze", "no_sacc", "still", "left", "right" Please refer to http://wiki.icub.org/iCub_documentation/group__actionsRenderingEngine.html)
-    * @param shouldWait is the function blocking ? 
+    * Bring the hand in the visual field and move it with the 
+    * purpose of visual exploration.
+    * @param opts Options of ARE commands ("no_head", "no_gaze", 
+    *             "no_sacc", "still", "left", "right").
+    * @param shouldWait is the function blocking? 
     * @return true in case of successfull motor command, false 
     *         otherwise.
     */
@@ -227,9 +249,11 @@ public:
     }
 
     /**
-    * The robot puts one arm forward with the palm of the hand facing up and waiting for an object to be put on it.
-    * @param opts Options of ARE commands ("no_head", "no_gaze", "no_sacc", "still", "left", "right" Please refer to http://wiki.icub.org/iCub_documentation/group__actionsRenderingEngine.html)
-    * @param shouldWait is the function blocking ? 
+    * Put one arm forward with the palm of the hand facing up and
+    * wait for an object. 
+    * @param opts Options of ARE commands ("no_head", "no_gaze", 
+    *             "no_sacc", "still", "left", "right").
+    * @param shouldWait is the function blocking? 
     * @return true in case of successfull motor command, false 
     *         otherwise.
     */
@@ -247,9 +271,12 @@ public:
     }
 
     /**
-    * The robot puts one arm forward with the palm of the hand facing up and opens the fingers so that the object held in the hand is free to be taken.
-    * @param opts Options of ARE commands ("no_head", "no_gaze", "no_sacc", "still", "left", "right" Please refer to http://wiki.icub.org/iCub_documentation/group__actionsRenderingEngine.html)
-    * @param shouldWait is the function blocking ? 
+    * Put one arm forward with the palm of the hand facing up and
+    * open the fingers so that the object held in the hand is free 
+    * to be taken. 
+    * @param opts Options of ARE commands ("no_head", "no_gaze", 
+    *             "no_sacc", "still", "left", "right").
+    * @param shouldWait is the function blocking?
     * @return true in case of successfull motor command, false 
     *         otherwise.
     */
