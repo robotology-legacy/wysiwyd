e48acc2865cd2f073365a46aa0f76e285fcf4c1c
gregoire-pointeau <gregoire.pointeau@inserm.fr>
2014-09-25 14:50:34 +0200

    abmReasoning
    knownInteraction cleaning

--- main/src/modules/abm/abmReasoning/include/knownInteraction.h
+++ main/src/modules/abm/abmReasoning/include/knownInteraction.h
@@ -11,7 +11,24 @@ class knownInteraction
 public:
     string                                          sSubject;
     vector< tuple<string, int, string, string> >            listInteraction;        // an interaction : argument (name of the "object", number of time, type of "interaction", role of the interaction "spatial..."
-    void    addInteraction(tuple<string, int, string, string>);
+
+
+    void addInteraction(tuple<string, int, string, string> tInput)
+    {
+        bool bFound = false;
+        for (vector<tuple<string, int, string, string>>::iterator itTuple = listInteraction.begin() ; itTuple != listInteraction.end() ; itTuple++)
+        {
+            if (get<0>(tInput) == get<0>(*itTuple) && get<2>(tInput) == get<2>(*itTuple) && get<3>(tInput) == get<3>(*itTuple) && !bFound) 
+            {
+                get<1>(*itTuple)++;
+                bFound =true;
+            }
+        }
+        if (!bFound)
+        {
+            listInteraction.push_back(tInput);
+        }
+    }
 
 };
 
