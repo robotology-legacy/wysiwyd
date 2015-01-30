1f80da6216b754a07b003d28d03f3beca6fed993
Tobias Fischer <t.fischer@imperial.ac.uk>
2015-01-30 14:51:12 +0000

    [ABM] Add copyright information

--- main/src/modules/abm/autobiographicalMemory/src/helpers.cpp
+++ main/src/modules/abm/autobiographicalMemory/src/helpers.cpp
@@ -1,3 +1,21 @@
+/*
+ *
+ * Copyright (C) 2015 WYSIWYD Consortium, European Commission FP7 Project ICT-612139
+ * Authors: Tobias Fischer, Grégoire Pointeau, Maxime Petit
+ * email:   t.fischer@imperial.ac.uk, greg.pointeau@gmail.com, m.petit@imperial.ac.uk
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * wysiwyd/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
 #include "autobiographicalMemory.h"
 
 #ifdef WIN32
--- main/src/modules/abm/autobiographicalMemory/src/visualABM.cpp
+++ main/src/modules/abm/autobiographicalMemory/src/visualABM.cpp
@@ -1,3 +1,21 @@
+/*
+ *
+ * Copyright (C) 2015 WYSIWYD Consortium, European Commission FP7 Project ICT-612139
+ * Authors: Tobias Fischer, Maxime Petit
+ * email:   t.fischer@imperial.ac.uk, m.petit@imperial.ac.uk
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * wysiwyd/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
 #include <yarp/sig/all.h>
 #include "autobiographicalMemory.h"
 
