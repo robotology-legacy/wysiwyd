1a04a6ab8633c9a95dadad5fee6c1db369b8b198
Ugo Pattacini <ugo.pattacini@iit.it>
2015-04-23 16:05:51 +0100

    .gitignore modified; auto-generated *.cmake file added to repo

---
+++ main/src/idl_generated_code/slidingController_thrift.cmake
@@ -0,0 +1,5 @@
+## This is an automatically-generated file.
+## It could get re-generated if the ALLOW_IDL_GENERATION flag is on
+
+set(headers include/slidingController_IDL.h)
+set(sources src/slidingController_IDL.cpp)
