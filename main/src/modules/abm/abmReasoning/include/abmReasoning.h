d7482c6a771975d2f7ac8968af5cd9844debdd5f
gregoire-pointeau <gregoire.pointeau@inserm.fr>
2015-06-22 14:18:39 +0200

    [abmReasoning] add iCubClient

--- main/src/modules/abm/abmReasoning/include/abmReasoning.h
+++ main/src/modules/abm/abmReasoning/include/abmReasoning.h
@@ -6,6 +6,8 @@ class abmReasoning : public yarp::os::RFModule
 {
 private:
 
+
+    ICubClient *iCub;
     // Internal member, module-related :
     abmReasoningFunction *iFunction;            //  class of generic functions
     std::string  savefile;
