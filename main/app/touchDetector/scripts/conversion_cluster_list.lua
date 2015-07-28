61d26d68f0e6a5de72b5e4d22d1c14973387234d
Bertrand HIGY <bertrand.higy@gmail.com>
2015-07-28 14:27:53 +0200

    [touchDetector] conversion - sending nothing when no activation

--- main/app/touchDetector/scripts/conversion_cluster_list.lua
+++ main/app/touchDetector/scripts/conversion_cluster_list.lua
@@ -7,21 +7,25 @@
 -- loading lua-yarp binding library
 require("yarp")
 
-PortMonitor.update = function(thing)
+PortMonitor.accept = function(thing)
 	if thing:asBottle() == nil then
         print("event_monitor.lua: got wrong data type (expected type Bottle)")
         return thing
     end
     
 	bt = thing:asBottle()
-	output = yarp.Bottle()
+	PortMonitor.output = yarp.Bottle()
 	for i = 0, bt:size() - 1 do
 	   	if (bt:get(i):asInt() > 3) then
-	   		output:addInt(i);
+	   		PortMonitor.output:addInt(i);
 	   	end
 	end
 	
+	return PortMonitor.output:size() > 0;
+end
+
+PortMonitor.update = function(thing)
 	th = yarp.Things()
-    th:setPortWriter(output)
+    th:setPortWriter(PortMonitor.output)
 	return th
 end
