7a3baef572c3e0bce49abb07993f019bb60e3888
gregoire-pointeau <gregoire.pointeau@inserm.fr>
2015-11-02 09:50:09 +0100

    [abmReasoning]  narrativeRelated

---
+++ main/src/modules/abm/abmReasoning/src/narrativeRelated.cpp
@@ -0,0 +1,25 @@
+/*
+*
+* Copyright (C) 2015 WYSIWYD Consortium, European Commission FP7 Project ICT-612139
+* Authors: Grégoire Pointeau
+* email:   gregoire.pointeau@inserm.fr
+* Permission is granted to copy, distribute, and/or modify this program
+* under the terms of the GNU General Public License, version 2 or any
+* later version published by the Free Software Foundation.
+*
+* A copy of the license can be found at
+* wysiwyd/license/gpl.txt
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+* Public License for more details
+*/
+
+#include <abmReasoning.h>
+
+
+using namespace yarp::os;
+using namespace wysiwyd::wrdac;
+using namespace std;
+
