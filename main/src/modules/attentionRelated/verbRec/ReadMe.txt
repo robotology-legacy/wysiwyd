f4544c0c9ad1acb4b8a95d72237fe348774c26b1
MagnusJohnsson <amtjohnsson@gmail.com>
2015-11-05 14:52:11 +0100

    added new version of verbRec 2015-11-05

---
+++ main/src/modules/attentionRelated/verbRec/ReadMe.txt
@@ -0,0 +1,9 @@
+* In a first console: Launch a yarpserver.
+
+* In a second console: Launch yarpdataplayer-gtk and open dumpHuman wave
+
+* In a third console: yarp read /output_data
+
+* In a fourth console: /wysiwyd/main/build/bin$ ./verbRec
+
+* Run yarpdataplayer-gtk 
--- main/src/modules/attentionRelated/verbRec/include/verbRec.h
+++ main/src/modules/attentionRelated/verbRec/include/verbRec.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2015 WYSIWYD Consortium, European Commission FP7 Project ICT-612139
+ * Authors: Magnus Johnsson and Zahra Gharaee
+ * email:   magnus@magnusjohnsson.se zahra.gharaee@gmail.com
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * wysiwyd/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
 #include <math.h>
 #include <iostream>
 #include <iomanip>
@@ -17,6 +34,12 @@ private:
     	int count;
 
 	float input[47];
+	float output[11];
+
+	int r_has_obj[4];
+	int a_has_obj[4];
+
+	ConstString objectNames[4];
 
 public:
     	bool configure(yarp::os::ResourceFinder &rf);
@@ -41,4 +64,25 @@ public:
 	void readData(const Bottle& command, float* input);
 
 	bool wave();
+
+	bool move_obj1();
+	bool move_obj2();
+	bool move_obj3();
+	bool move_obj4();
+	bool move(int* object);
+
+
+	bool take(int* object);
+//	bool put(int* object);
+	int put();
+	bool push(int* object);
+	bool pull(int* object);
+	bool point(int* object);
+
+//	bool lift(int* object);
+	int lift();
+//	bool have(int* object);
+	int have(/*int ra, int obj*/);
+	bool grasp(int* object);
+	bool give(int* object);
 };
