1d16ec3269b46c3ab7af25dfc79ceaa3eb6c0236
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2016-01-20 13:13:22 +0100

    [reactiveLayer] first working version of allo plotter

--- main/src/modules/reactiveLayer/plotter.py
+++ main/src/modules/reactiveLayer/plotter.py
@@ -1,5 +1,7 @@
 from time import sleep
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 import yarp
 
 
@@ -48,18 +50,57 @@ if __name__ == '__main__':
         drive_value_ports[i].open("/" + module_name +"/" + d + ":i") # Open port (name)
         print yarp.Network.connect("/homeostasis/" + d + "/max:o", drive_value_ports[i].getName())
 
-    x = range(200)
-    drive_value_history = []
-    while True:
-        drive_values = []
-        for port, homeo_max in zip(drive_value_ports, homeo_maxs):
+    win_size = 200
+
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0, win_size), ylim=(0, 1))
+    colors = 'g', 'b', 'w', 'k', 'r', 'y', 'c', 'm'
+    value_lines = [ax.plot([], [], colors[i] + '-', lw=2) for i in range(len(drives))]
+
+
+    drive_values = [[0.] * win_size for _ in drives]
+    # drive_value_history = [drive_values for _ in range(win_size)]
+
+    # def init():
+    #     for line in value_lines:
+    #         line.set_data([], [])
+    #     return value_lines
+
+    def animate(t, drive_values_as_list):
+        # print drive_values_as_list[0]
+        drive_values_as_list[0] = [values[1:] + [0.] for values in drive_values_as_list[0]]
+        # drive_value_history[:-1] = drive_value_history[1:]
+        for i, (port, homeo_max, line) in enumerate(zip(drive_value_ports, homeo_maxs, value_lines)):
             res = port.read()
             if res is not None:
-                drive_values.append(res.get(0).asDouble() + homeo_max)
+                drive_values_as_list[0][i][-1] = res.get(0).asDouble() + homeo_max
+                # drive_value_history[-1][i] = res.get(0).asDouble() + homeo_max
+                # drive_values.append(res.get(0).asDouble() + homeo_max)
             else:
-                drive_values.append(res)
-        drive_value_history.append(drive_values)
-        plt.plot(x, drive_value_history[-200:])
-        plt.draw()
-        yarp.Time.delay(0.1)
+                drive_values_as_list[0][i][-1] = res
+                # drive_values.append(res)
+            # print line
+            line[0].set_data(range(win_size), drive_values_as_list[0][i])
+        return value_lines
+
+    # call the animator.  blit=True means only re-draw the parts that have changed.
+    anim = animation.FuncAnimation(fig, animate, fargs=([drive_values],), init_func=None,
+                               frames=None, interval=100)
+
+    plt.show()
+
+    # x = range(200)
+    # drive_value_history = []
+    # while True:
+    #     drive_values = []
+    #     for port, homeo_max in zip(drive_value_ports, homeo_maxs):
+    #         res = port.read()
+    #         if res is not None:
+    #             drive_values.append(res.get(0).asDouble() + homeo_max)
+    #         else:
+    #             drive_values.append(res)
+    #     drive_value_history.append(drive_values)
+    #     plt.plot(x, drive_value_history[-200:])
+    #     plt.draw()
+    #     yarp.Time.delay(0.1)
 
