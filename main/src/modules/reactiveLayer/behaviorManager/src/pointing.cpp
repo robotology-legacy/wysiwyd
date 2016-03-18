9ba57e9c9dbb687d679b7ca507d5b17f790e96da
sockchinglow <sockching.low@upf.edu>
2016-03-18 14:50:19 +0100

    [behaviorManager] look to partner during pointing behaviour

--- main/src/modules/reactiveLayer/behaviorManager/src/pointing.cpp
+++ main/src/modules/reactiveLayer/behaviorManager/src/pointing.cpp
@@ -8,28 +8,50 @@ void Pointing::configure() {
 }
 
 void Pointing::run(Bottle args/*=Bottle()*/) {
-    yInfo() << "Pointing::run";
-    Bottle *sensation = sensation_port_in.read();
-    
-    int id = yarp::os::Random::uniform(0, sensation->size()-1);
-    yDebug()<<"Randomly selected: "<< id; // should be random here 
-    string obj_name = sensation->get(id).asList()->get(1).asString();
-    
-    iCub->say("I could point to the " + obj_name);
-    Time::delay(2.0);
-    // yInfo() << "About to point the " + obj_name;
-    // return;
+	yInfo() << "Pointing::run";
+	Bottle *sensation = sensation_port_in.read();
 
-    iCub->say("Do you know that this is a " + obj_name);
-    Object* obj = iCub->opc->addOrRetrieveEntity<Object>(obj_name);
-    string sHand = "right";
-    if (obj->m_ego_position[1]<0) 
-        sHand = "left";
+	int id = yarp::os::Random::uniform(0, sensation->size() - 1);
+	yDebug() << "Randomly selected: " << id; // should be random here 
+	string obj_name = sensation->get(id).asList()->get(1).asString();
 
-    Bottle bHand(sHand); 
+	iCub->say("I could point to the " + obj_name);
+	Time::delay(2.0);
+	// yInfo() << "About to point the " + obj_name;
+	// return;
 
-    bool succeeded = iCub->point(obj_name, bHand);
-    Time::delay(0.5);
+	Object* obj = iCub->opc->addOrRetrieveEntity<Object>(obj_name);
+	string sHand = "right";
+	if (obj->m_ego_position[1] < 0)
+		sHand = "left";
+
+	Bottle bHand(sHand);
+
+	bool succeeded = iCub->point(obj_name, bHand);
+	Time::delay(0.5);
+
+	iCub->opc->checkout();
+	yInfo() << "[pointing] : opc checkout";
+	list<Entity*> lEntities = iCub->opc->EntitiesCache();
+	string aName = "";
+	for (auto& entity : lEntities)
+	{
+		if (entity->entity_type() == "agent")
+		{
+			aName = entity->name();
+		}
+	}
+	if (aName == "")
+	{
+		yInfo() << "No agents detected, looking action aborted.";
+	}
+	else
+	{
+		iCub->look(aName);
+		Time::delay(0.3);
+	}
+
+    iCub->say("Do you know that this is a " + obj_name, false);
 
     if (!succeeded) {
         iCub->say(" I couldn't find the " + obj_name);
