2e0681fde455340d2624e52f0f44907f07de8e94
Tobias Fischer <t.fischer@imperial.ac.uk>
2016-04-14 19:22:23 +0200

    [ABM] Fix previous commit

--- main/src/modules/abm/autobiographicalMemory/src/autobiographicalMemory.cpp
+++ main/src/modules/abm/autobiographicalMemory/src/autobiographicalMemory.cpp
@@ -810,10 +810,10 @@ bool autobiographicalMemory::updateModule() {
 
         //init of the stream record done: go through the classic record phase
         streamStatus = "record";
+        yDebug() << "[mutexChangeover] unlocked in begin";
         mutexChangeover.unlock();
     }
     else if (streamStatus == "record") {
-        yDebug() << "[mutexChangeover] unlocked in record";
         string synchroTime = getCurrentTime();
         storeImagesAndData(synchroTime);
     }
