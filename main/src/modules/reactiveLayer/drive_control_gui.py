b13b3f3d189bf869bcf54ee278cc18062ad90a49
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2016-04-15 14:57:10 +0200

    [reactiveLayer] Fix drive control gui button order

--- main/src/modules/reactiveLayer/drive_control_gui.py
+++ main/src/modules/reactiveLayer/drive_control_gui.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
+from collections import OrderedDict
 from Tkinter import *
 
-from drive_control_rpc import pointing, tagging, reset_all, freeze_all, unfreeze_all, narrate, manual_mode, automatic_mode
+from drive_control_rpc import pointing, tagging, dummy, reset_all, freeze_all, unfreeze_all, narrate, manual_mode, automatic_mode
 
 window = Tk()
 
@@ -16,16 +17,17 @@ title = Label(window, text="Manual drive control")
 
 title.pack()
 
-buttons = {}
+buttons = OrderedDict()
 
 buttons["Manual mode"] = Button(window, text="Manual mode", command=manual_mode)
 buttons["Autonomous mode"] = Button(window, text="Autonomous mode", command=automatic_mode)
+buttons["freeze"] = Button(window, text="Freeze", command=freeze_all)
+buttons["unfreeze"] = Button(window, text="Unfreeze", command=unfreeze_all)
+buttons["reset"] = Button(window, text="Reset", command=reset_all)
 buttons["pointing"] = Button(window, text="Pointing", command=pointing)
 buttons["tagging"] = Button(window, text="Tagging", command=tagging)
-buttons["narate"] = Button(window, text="narrate", command=narrate)
-buttons["freeze"] = Button(window, text="freeze", command=freeze_all)
-buttons["unfreeze"] = Button(window, text="unfreeze", command=unfreeze_all)
-buttons["reset"] = Button(window, text="reset", command=reset_all)
+buttons["narate"] = Button(window, text="Narrate", command=narrate)
+buttons["dummy"] = Button(window, text="Dummy", command=dummy)
 
 
 for b in buttons.values():
