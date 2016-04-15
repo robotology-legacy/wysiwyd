783c4022442eaf427f05f9d6abaa7ccc7105663f
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2016-04-15 17:26:26 +0200

    [reactiveLayer] revert changes made in drive_control_gui
    cancel 29b224d90a201a0b55c502568390dde4083762f8

--- main/src/modules/reactiveLayer/drive_control_rpc.py
+++ main/src/modules/reactiveLayer/drive_control_rpc.py
@@ -7,56 +7,20 @@ yarp.Network.init()
 
 # Open the RPC port
 toHomeo = yarp.Port()
-homeoPortName="/driveControl/toHomeostasis/rpc"+":o"
+homeoPortName="/manager/toHomeostasis/rpc"+":o"#"/NNsound:i"    
 toHomeo.open(homeoPortName)
 
 toAllo = yarp.Port()
-alloPortName="/driveControl/toAllostasis/rpc"+":o"
+alloPortName="/manager/toAllostasis/rpc"+":o"#"/NNsound:i"    
 toAllo.open(alloPortName)
 
 homeoRPC = "/homeostasis/rpc"
 alloRPC = "/AllostaticController/rpc"
-behaviorRPC = "/BehaviorManager/trigger:i"
-
-toBehaviorManager = yarp.Port()
-toBehaviorManager.open("/driveControl/to_behaviorManager_rpc")
 
 print yarp.Network.connect(homeoPortName,homeoRPC)
 print yarp.Network.connect(alloPortName,alloRPC)
-print yarp.Network.connect(toBehaviorManager.getName(), behaviorRPC)
 
-request = yarp.Bottle()
-rep = yarp.Bottle()
 
-# Retrieve behavior names from the behaviorManager module
-request.clear()
-rep.clear()
-request.addString("names")
-toBehaviorManager.write(request, rep)
-behaviors = []
-behavior_ports = {}
-names = rep.get(0).asList()
-for i in range(names.size()):
-	name = names.get(i).asString()
-	behaviors.append(name)
-	behavior_ports[name] = yarp.BufferedPortBottle()
-	behavior_ports[name].open("/driveControl/behaviors/" + behaviors[-1] + ":i")
-	yarp.Network.connect("/BehaviorManager/" + behaviors[-1] + "/start_stop:o", behavior_ports[name].getName())
-
-def close_ports():
-	print "Interrupting and closing ports"
-	# interrupting
-	toHomeo.interrupt()
-	toAllo.interrupt()
-	toBehaviorManager.interrupt()
-	for beh_port in behavior_ports.values():
-		beh_port.interrupt()
-	# closing
-	toHomeo.close()
-	toAllo.close()
-	toBehaviorManager.close()
-	for beh_port in behavior_ports.values():
-		beh_port.close()
 
 
 def trigger_behavior(behavior):
@@ -70,24 +34,10 @@ def trigger_behavior(behavior):
 	if not yarp.Network.isConnected(homeoPortName,homeoRPC):
 		print yarp.Network.connect(homeoPortName,homeoRPC)
 		yarp.Time.delay(0.1)
-	if not yarp.Network.isConnected("/BehaviorManager/" + behavior + "/start_stop:o", behavior_ports[behavior].getName()):
-		print yarp.Network.connect("/BehaviorManager/" + behavior + "/start_stop:o", behavior_ports[behavior].getName())
-		yarp.Time.delay(0.1)	
 	toHomeo.write(cmd)
-	yarp.Time.delay(0.5)
+	yarp.Time.delay(2.)
 	reset_all()
 	freeze_all()	
-	res = behavior_ports[behavior].read(False)
-	while res is None and msg != "stop":
-		print "while"
-		res = behavior_ports[behavior].read(False)
-		if res is not None:
-			msg = res.get(0).asString()
-			if msg == "stop":
-				reset_all()
-				freeze_all()			
-		yarp.Time.delay(0.1)
-		
 
 def freeze_all():
 	# Prepare command
@@ -137,9 +87,6 @@ def tagging():
 
 def test():
 	trigger_behavior("test")
-
-def dummy():
-	trigger_behavior("dummy")
 
 def manual_mode():
 	# Prepare command
