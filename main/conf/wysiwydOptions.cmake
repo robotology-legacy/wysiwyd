39d452aae6f161cc7df1909687559433679578e6
Tobias Fischer <t.fischer@imperial.ac.uk>
2016-05-12 15:59:59 +0100

    Fixes #131 Adds -Wno-deprecated-declarations to warnings list to ignore this warning type

--- main/conf/wysiwydOptions.cmake
+++ main/conf/wysiwydOptions.cmake
@@ -3,23 +3,24 @@
 # CopyPolicy: Released under the terms of the GNU GPL v2.0.
 
 if(MSVC)
- 
+
    if(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
       string(REGEX REPLACE "/W[0-4]" "/W2" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
    else()
       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W2")
    endif()
- 
+
    add_definitions(-D_CRT_SECURE_NO_DEPRECATE -D_CRT_NONSTDC_NO_DEPRECATE)
    set(CMAKE_DEBUG_POSTFIX "d")
- 
+
 elseif(CMAKE_COMPILER_IS_GNUCXX OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
- 
+
    set(WARNINGS_LIST -Wall)
    foreach(WARNING ${WARNINGS_LIST})
       set(WARNINGS_STRING "${WARNINGS_STRING} ${WARNING}")
    endforeach(WARNING)
- 
+   set(WARNINGS_STRING "${WARNINGS_STRING} -Wno-deprecated-declarations")
+
    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${WARNINGS_STRING}")
 
    include(CheckCXXCompilerFlag)
@@ -45,5 +46,3 @@ if(NOT CMAKE_CONFIGURATION_TYPES)
     set(WYSIWYD_BUILD_TYPES "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
     set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS ${WYSIWYD_BUILD_TYPES})
 endif()
-
-
