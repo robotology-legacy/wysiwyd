0fc049c41ac91b2dab4d5c1a55bcbea931f3c422
dcam0050 <daniel.james.camilleri.13@gmail.com>
2016-09-02 13:49:25 +0100

    finalized files

--- main/src/modules/samRelated/SAM/SAM_Drivers/__init__.py
+++ main/src/modules/samRelated/SAM/SAM_Drivers/__init__.py
@@ -1,14 +1,6 @@
 # Copyright (c) 2015, Andreas Damianou
-
-import matplotlib as mp
-# Use this backend for when the server updates plots through the X 
-#mp.use('TkAgg')
-import numpy as np
-import pylab as pb
-from . import SAM_Core
-from . import SAM_Drivers
-#from . import SAM_Demos
-
-pb.ion()
-
-default_seed = 123344
+# from .SAMDriver_speech import *
+from .SAMDriver_AR import *
+from .SAMDriver_temporalActions import *
+from .SAMDriver_temporal import *
+from .SAMDriver_interaction import *
