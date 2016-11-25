7c990c359236189423c6c1fae3b3e93ed721923c
Tobias Fischer <t.fischer@imperial.ac.uk>
2016-11-25 11:17:26 +0000

    Small update in ARE_KARMAExample

--- main/src/examples/ARE_KARMAiCubClientExample/main.cpp
+++ main/src/examples/ARE_KARMAiCubClientExample/main.cpp
@@ -38,7 +38,7 @@ int main()
         return -1;
     }
 
-    ICubClient iCub("KARMAiCubClientExample","icubClient","example_ARE_KARMA.ini");
+    ICubClient iCub("ARE_KARMAiCubClientExample","icubClient","example_ARE_KARMA.ini");
 
     // we connect just to ARE (skip connecting to OPC)
     // we need connect to KARMA also
@@ -50,8 +50,8 @@ int main()
 
     // object location in the iCub frame
     Vector x(4);
-    x[0]=-0.35;
-    x[1]=0.15;
+    x[0]=-0.25;
+    x[1]=-0.20;
     x[2]=-0.05;
     x[3]=1.0;
 
@@ -61,25 +61,25 @@ int main()
     T(1,0)=0.0;  T(1,1)=0.0;  T(1,2)=1.0; T(1,3)=0.5976;
     T(2,0)=-1.0; T(2,1)=0.0;  T(2,2)=0.0; T(2,3)=-0.026;
     T(3,0)=0.0;  T(3,1)=0.0;  T(3,2)=0.0; T(3,3)=1.0;
-    Vector wx=T*x;
+    Vector wx=T*x; //if want to create the object in the simulator
     x.pop_back();
 
     bool ok ;
 
     iCub.home();    // Home by using ARE
-    x[1] =x[1] - 0.1;
     cout<<"try to pushFront with KARMA ..."<<endl;
-    ok = iCub.pushKarmaFront(x,-0.35);
+    ok = iCub.pushKarmaFront(x,-0.35);                  //WARNING: second argument is the desired x[0] that we want and should be less than original x[0]
     cout<<(ok?"success":"failed")<<endl;
     Time::delay(4.0);
 
-    iCub.home();    // Home by using ARE
-    x[1] =x[1] + 0.1;
-    //x[2] =x[2] - 0.1;
+    /*iCub.home();    // Home by using ARE
+    x[1] =x[1] - 0.1;                        //offset on lateral to draw?
     cout<<"try to pull with KARMA...";
-    ok = iCub.drawKarma(x,0,0.2,0.2);
+    ok = iCub.drawKarma(x,0,0.1,0.1);        //draw of 0.1m but the objects is considered as -0.35 so final: -0.25
     cout<<(ok?"success":"failed")<<endl;
     Time::delay(4.0);
+    x[1] = x[1] + 0.1; //go back to original value
+    */
 
 
     //----------------------------> ARE related action
