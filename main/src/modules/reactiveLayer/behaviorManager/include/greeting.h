20146fc0818338059ab943cb0ca578fc0d387187
sock <sockching.low@upf.edu>
2016-12-09 19:11:23 +0100

    [behaviorManager] debug greeting header file

--- main/src/modules/reactiveLayer/behaviorManager/include/greeting.h
+++ main/src/modules/reactiveLayer/behaviorManager/include/greeting.h
@@ -3,15 +3,30 @@
 #include <iomanip>
 #include <yarp/os/all.h>
 #include <yarp/sig/all.h>
+
+#include <yarp/math/SVD.h>
+#include <yarp/math/Rand.h>
+#include "wrdac/clients/icubClient.h"
 #include <map>
 
-
 #include "behavior.h"
+
+using namespace std;
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::math;
+using namespace wysiwyd::wrdac;
 
 class Greeting: public Behavior
 {
 public:
     void configure();
     void run(Bottle args=Bottle());
+    Greeting(Mutex* mut, ResourceFinder &rf, std::string behaviorName): Behavior(mut, rf, behaviorName) {
+        ;
+    }
+    void close_extra_ports() {
+        ;
+    }
 };
 
